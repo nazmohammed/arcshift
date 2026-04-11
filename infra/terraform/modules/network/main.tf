7e187bf52ed5748b41b815181d285a3ee7122b11
Nazeer Mohammed <manucian.mohammed@outlook.com>
2026-04-11 16:51:03 +0300

    feat: add Terraform network module with VNet and subnets

---
+++ infra/terraform/modules/network/main.tf
@@ -0,0 +1,68 @@
+variable "app_name" {
+  type = string
+}
+
+variable "location" {
+  type = string
+}
+
+variable "tags" {
+  type = map(string)
+}
+
+variable "resource_group_name" {
+  type = string
+}
+
+resource "azurerm_virtual_network" "main" {
+  name                = "vnet-${var.app_name}"
+  location            = var.location
+  resource_group_name = var.resource_group_name
+  address_space       = ["10.0.0.0/16"]
+  tags                = var.tags
+}
+
+resource "azurerm_subnet" "app" {
+  name                 = "snet-app"
+  resource_group_name  = var.resource_group_name
+  virtual_network_name = azurerm_virtual_network.main.name
+  address_prefixes     = ["10.0.1.0/24"]
+
+  delegation {
+    name = "containerApps"
+    service_delegation {
+      name = "Microsoft.App/environments"
+    }
+  }
+}
+
+resource "azurerm_subnet" "sql" {
+  name                 = "snet-sql"
+  resource_group_name  = var.resource_group_name
+  virtual_network_name = azurerm_virtual_network.main.name
+  address_prefixes     = ["10.0.2.0/24"]
+  service_endpoints    = ["Microsoft.Sql"]
+}
+
+resource "azurerm_subnet" "private_endpoints" {
+  name                 = "snet-private-endpoints"
+  resource_group_name  = var.resource_group_name
+  virtual_network_name = azurerm_virtual_network.main.name
+  address_prefixes     = ["10.0.3.0/24"]
+}
+
+output "vnet_id" {
+  value = azurerm_virtual_network.main.id
+}
+
+output "app_subnet_id" {
+  value = azurerm_subnet.app.id
+}
+
+output "sql_subnet_id" {
+  value = azurerm_subnet.sql.id
+}
+
+output "private_endpoint_subnet_id" {
+  value = azurerm_subnet.private_endpoints.id
+}
